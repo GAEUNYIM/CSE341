cfc708fbadb475864297f22ea928a42b85e92d96
Jooyong Yi <jooyong@unist.ac.kr>
2020-03-23 06:42:55 +0000

    added a Java example

---
+++ Java/Add.java
@@ -0,0 +1,10 @@
+public class Add {
+
+    static int perform(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1 + 2 = " + Add.perform(1, 2));
+    }
+}
